02fef09cefb46ef6058050186fc49c99524cb315
Anshul <anshulkhare7@gmail.com>
2020-03-11 14:18:35 +0530

    added skeleton

---
+++ BinaryTreeLevelOrder/BinaryTreeLevelOrder.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class BinaryTreeLevelOrder {
+
+    public List<Integer> getLevelAverages(TreeNode root){
+        return new ArrayList<Integer>();
+    }
+
+    public List<Integer> getLevelMax(TreeNode root){
+        return new ArrayList<Integer>();
+    }
+
+    public List<List<Integer>> getLevel(TreeNode root){
+        return new ArrayList<List<Integer>>();
+    }
+
+    public List<List<Integer>> getLevelReverse(TreeNode root){
+        return new ArrayList<List<Integer>>();
+    }
+
+    public List<List<Integer>> getLevelZigzag(TreeNode root){
+        return new ArrayList<List<Integer>>();
+    }
+
+    public Integer getMinimumDepth(TreeNode root){
+        return new Integer(0);
+    }
+}
