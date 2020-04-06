6b715a7e1cc493ea8474699c7e5936dbedb6c314
Anshul <anshulkhare7@gmail.com>
2020-04-06 12:43:21 +0530

    Solution implemented

--- MatrixManipulation/LuckyNumber.java
+++ MatrixManipulation/LuckyNumber.java
@@ -1,7 +1,41 @@
+import java.util.*;
 
 public class LuckyNumber {
 
-    public int[] luckyNumbers(int[][] indices) {
-        return new int[] {-1};
+    public List<Integer> luckyNumbers(int[][] indices) {
+        
+        List<Integer> luckNumbers = new ArrayList<>();
+
+        int rows = indices.length;
+        int cols = indices[0].length;
+        
+        int[] rowList = new int[rows];
+        int[] colList = new int[cols];
+
+        Arrays.fill(rowList, Integer.MAX_VALUE);
+        Arrays.fill(colList, Integer.MIN_VALUE);
+
+        
+
+        for(int i=0; i < rows; i ++){
+            for(int j=0; j < cols; j++){
+                rowList[i] = Math.min(indices[i][j], rowList[i]);
+                colList[j] = Math.max(indices[i][j], colList[j]);
+            }
+        }
+        
+        for(int i=0; i < rowList.length; i++)   {
+            for(int j=0; j < colList.length; j++){
+                if(rowList[i]==colList[j]){
+                    luckNumbers.add(rowList[i]);
+                }
+            }
+        }
+
+
+        return luckNumbers;
+    }    
+
+    public static void main(String[] args) {
     }
 }
--- MatrixManipulation/LuckyNumberTest.java
+++ MatrixManipulation/LuckyNumberTest.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class LuckyNumberTest {
 
@@ -10,16 +10,23 @@ public class LuckyNumberTest {
     }
 
     private void testLuckyNumber(){ 
-        int[][] matrix = new int[][] {new int[]{3,7,8},new int[] {9,11,13}, new int[] {15,16,17}};
+        int[][] matrix1 = new int[][] {new int[]{3,7,8},new int[] {9,11,13}, new int[] {15,16,17}};
+        int[][] matrix2 = new int[][] {new int[]{1,10,4,2},new int[] {9,3,8,7}, new int[] {15, 16, 17, 12}};
 
         LuckyNumber ln = new LuckyNumber();
-        int[] expected = new int[] {15};
-        int[] actual = ln.luckyNumbers(matrix);
+        List<Integer> expected = new ArrayList<Integer>();
+        expected.add(15);
+        // expected.add(12);
+        
+        List<Integer> actual = ln.luckyNumbers(matrix1);
+        // List<Integer> actual = ln.luckyNumbers(matrix2);
 
-        if(Arrays.asList(expected).equals(Arrays.asList(actual))){
-            System.out.println("Test Case testOddCelss Passed.");
+        if(expected.equals(actual)){
+            System.out.println("Test Case testLuckyNumber Passed.");
         }else{
-            System.out.println("Test Case testOddCelss FAILED. Expected: "+Arrays.toString(expected)+" Actual: "+Arrays.toString(actual)); 
+            System.out.println("Test Case testLuckyNumber FAILED. Expected: "+expected+" Actual: "+actual); 
         }
     }
+
+
 }
