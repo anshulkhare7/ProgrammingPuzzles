eb72d17cb37076b89c5bdc5f12b7751ae264882d
Anshul <anshulkhare7@gmail.com>
2020-04-16 15:30:32 +0530

    implemented solution

--- Arrays/SmallestSubArray.java
+++ Arrays/SmallestSubArray.java
@@ -6,7 +6,24 @@ public class SmallestSubArray {
     }
 
     public int solution(int[] input, int sum){
-        return -1;
+        if(input.length < 1)
+            return -1;
+        
+        int minLength = Integer.MAX_VALUE;
+        int startP = 0;
+        int endP = 0;
+        int runningSum = 0;
+
+        while(endP < input.length){            
+            runningSum = runningSum + input[endP];
+            while(runningSum>=sum){
+                minLength = Math.min(minLength, endP - startP + 1);
+                runningSum = runningSum - input[startP++];
+            }
+            endP++;
+        }        
+
+        return (minLength == Integer.MAX_VALUE) ? 0 : minLength;
     }
 
 }
