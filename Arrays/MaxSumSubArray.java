bc1a7400f7f4104b04f883aa499b4879c4946a85
Anshul <anshulkhare7@gmail.com>
2020-04-16 15:30:32 +0530

    implemented solution

--- Arrays/MaxSumSubArray.java
+++ Arrays/MaxSumSubArray.java
@@ -6,6 +6,25 @@ public class MaxSumSubArray {
     }
 
     public int solution(int[] input, int k){
-        return -1;
+        if(input.length < k)
+            return -1;
+        
+        int startP = 0;
+        int endP = startP + k;
+        int sum = 0;
+
+        for(int i=startP; i< endP; i++){
+            sum = sum + input[i];
+        }
+
+        int maxSum = sum;
+        while(endP < input.length){
+            sum = sum + input[endP] - input[startP];
+            maxSum = Math.max(sum, maxSum);
+            endP++;
+            startP++;
+        }
+
+        return maxSum;
     }
 }
